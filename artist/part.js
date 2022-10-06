968c6144fc1153480bf24f391c5f50faaec66292
Lê Anh Tuấn <anhtuan.freelc@gmail.com>
2022-10-06 23:34:25 +0700

    music-player
    update

---
+++ artist/part.js
@@ -0,0 +1,297 @@
+const data = JSON.parse(localStorage.getItem('data')) // lay data tu app.js 
+const artistIndex = JSON.parse(localStorage.getItem('index')) // lay index tu slick slider  
+const arTist = data.allArtist
+const songs = data.songs
+// du lieu tu localStorage
+const currentIndex = artistIndex
+
+const I = document.querySelector.bind(document)
+const II = document.querySelectorAll.bind(document)
+
+const audio = I('#audio')
+const thumb = I('.img-thumb')
+const cdThumb = I('.music-thumb')
+const nameSong = I('.music-name')
+const singer = I('.music-singer')
+const btnPlay = I('.btn-toggle-play')
+const inputRange = I('#range')
+const btnRandom = I('.btn-random')
+const btnRepeat = I('.btn-repeat')
+const playlistModel = I('.playlist')
+const playlistSong = I('.playlist-song')
+const allDuration = II('.time-duration')
+const itemMenu = II('.menu-nav  li')
+const menuSong = I('.music')
+
+
+const playlists = songs.filter((song) => {
+   if (artistIndex >= 0) {
+      return song.id_singer == artistIndex// tim ra nhung thang co id_singer dua vao index cua artist 
+
+   }
+})
+
+// render thumb
+function loadingSong(index) {
+   thumb.src = arTist[currentIndex].img
+   nameSong.textContent = arTist[currentIndex].content
+   singer.textContent = arTist[currentIndex].singer
+   if (I('.playlist-song:not(active)')) { // Khi chua click vao playlist thi mặc định nó sẽ phát bài đầu tiên trong danh sách  
+      audio.src = playlists[0].path
+      console.log(audio.src)
+   }
+   else if (I('.playlist-song.active')) {
+      confirm('saime r')
+      audio.src = playlists[index].path
+
+   }
+}
+
+//render Playlist
+function renderPlayList() {
+   const playlists = songs.filter((song) => {
+      return song.id_singer === artistIndex // tim ra nhung thang co id_singer dua vao index cua artist 
+   })
+   console.log(playlists)
+   // lấy những thằng thỏa điều kiện để render ra ngoài
+   const htmls = playlists.map((item, index) => {
+      return `
+      <div data-index= ${index} class="playlist-song" >
+      <div class="playlist-img">
+          <img src=" ${item.img}" alt="" srcset="">
+      </div> 
+      <div class="playlist-content">
+          <h3> ${item.name}</h3>
+          <p> ${item.singer}</p> 
+      </div>  
+      <audio id="datas-${index}" src=" ${item.path}" ></audio> 
+
+      <span class="currentDuration datas-${index}"> 0:0</span>
+     
+      <div class="btn-heart ">  
+     <i class="fa-solid fa-heart"></i>
+      </div>
+  </div>  
+      `
+
+   })
+   playlistModel.innerHTML = htmls.join('')
+}
+
+
+
+function renderlistSong() {
+   const AllList = II('.playlist-song')
+   AllList.forEach((playlist, index) => {
+
+      playlist.onclick = function (e) {
+         const songNode = e.target.closest('.playlist-song:not(.active)')
+
+
+         if (e.target.closest('.btn-heart')) {
+            e.target.closest('.btn-heart').classList.toggle('active')
+         }
+         else if (songNode) {
+            if (songNode.getAttribute('data-index') || Number(songNode.dataset.index)) {
+               // điều kiện nếu data-index = currentIndex thì sẽ thêm active vào chính songnode đó
+               if (I('.playlist-song.active')) {
+                  I('.playlist-song.active').classList.remove('active') // xóa playlist-song có sẵn active
+                  songNode.classList.add('active')
+               }
+               else {
+                  songNode.classList.add('active') // khi chưa có active thì click sẽ thêm active vào chính node đó, khi có rồi nó sẽ chuyển sang logic trên
+               }
+            }
+
+            let index = songNode.getAttribute('data-index') || Number(songNode.dataset.index)
+            // Những thằng index được lấy ra bằng data-index chứa trong playlists 
+            console.log(index)
+            console.log(playlists[index])
+            audio.src = playlists[index].path
+            audio.play()
+         }
+      }
+   })
+
+}
+
+
+function renderDuration() {
+   //Xu li render() duration 
+   for (let i = 0; i < playlists.length; i++) { // lấy length của playlist in ra song nằm trong playlists
+      let audioDuration = I(`#datas-${i}`)
+      let tagDuration = I(`.datas-${i}`)
+      // lay tung gia tri audio va tag dựa vào index ở render() và gọi lại thông qua i ở for 
+      audioDuration.onloadeddata = function () {
+         // cho từng audio thuộc tính loadeddata để khi khởi động nó sẽ tựt thêm giá trị vào
+         let allTime = audioDuration.duration // lấy ra duration dựa vào từng audio
+         let minutes = Math.floor(allTime / 60)
+         let seconds = Math.floor(allTime - minutes * 60)
+         if (seconds < 10) {
+            seconds = `0${seconds}`
+
+         }
+         tagDuration.innerText = ` ${minutes} :  ${seconds}`
+         // in ra giá trị lấy từ audio
+      }
+   }
+
+
+}
+
+
+
+const isPlaying = false;
+
+const app = {
+   handleSong: function () {
+      const _this = this
+      //Xoay thumb 
+      const scrollThumb = cdThumb.animate(
+         [{
+            transform: 'rotate(360deg) scale(1)',
+
+         }
+         ],
+         {
+            duration: 10000,
+            iterations: Infinity
+         }
+      )
+      scrollThumb.pause()
+
+      // Khi dung` bai hat se goi lai no
+      const cancelThumb = cdThumb.animate(
+         [{
+            transform: 'rotate(360deg) scale(1)',
+         }
+         ],
+         {
+            duration: 300,
+            iterations: 1
+         }
+      )
+      // Khi cancel ket thuc thi no se tra ve border = 10px
+      function cancelFinish() {
+         cancelThumb.onfinish = function (e) {
+            cdThumb.style.borderRadius = 10 + 'px' 
+         }
+      }
+
+      // Xu li button Play pause 
+
+      btnPlay.onclick = function () {
+         let cdThumb = I('.music-thumb')
+
+
+         if (_this.isPlaying) {
+            audio.pause()
+            cancelThumb.play() 
+         }
+         else {
+            audio.play()
+            cdThumb.style.borderRadius = "100%"
+            cancelFinish()
+         }
+      }
+      audio.onplay = function () {
+         _this.isPlaying = true
+         btnPlay.classList.add('active')
+         scrollThumb.play()
+
+      }
+      audio.onpause = function () {
+         _this.isPlaying = false
+         btnPlay.classList.remove('active')
+         scrollThumb.cancel()
+      }
+
+      // Xu li range 
+
+      audio.ontimeupdate = function () {
+         const currentTime = audio.currentTime
+         const allTime = audio.duration
+         const minutes = Math.floor(audio.duration / 60)
+         const seconds = Math.floor(audio.duration - minutes * 60)
+         const newTime = Math.floor(currentTime / allTime * 100)
+         const minutesCurrent = Math.floor(audio.currentTime / 60)
+         const secondsCurrent = Math.floor(audio.currentTime - minutesCurrent * 60)
+
+         if (audio.duration) {
+            inputRange.value = newTime
+            I('.duration').textContent = `${minutes < 10 ? '0' + minutes : minutes} :
+              ${seconds < 10 ? '0' + seconds : seconds}`
+            I('.remainting').textContent = `${minutesCurrent < 10 ? '0' + minutesCurrent : minutesCurrent} :
+               ${secondsCurrent < 10 ? '0' + secondsCurrent : secondsCurrent}`
+         }
+         else {
+            inputRange.value = 0
+         }
+      }
+
+      inputRange.oninput = function (e) {
+         const timePercent = Math.floor(e.target.value / 100 * audio.duration)
+
+         audio.currentTime = timePercent
+      }
+      //Xu li end 
+      audio.onended = function () {
+         if (_this.isRepeat) {
+            audio.play()
+         }
+         else {
+            _this.ranDom()
+         }
+         if (_this.isRandom) {
+            this.ranDom()
+         }
+         else {
+         }
+      }
+      // Xu li random 
+      btnRandom.onclick = function () {
+         _this.isRandom = !_this.isRandom
+         btnRandom.classList.toggle('active')
+      }
+      // xu li Repeat 
+      btnRepeat.onclick = function () {
+         _this.isRepeat = !_this.isRepeat
+         btnRepeat.classList.toggle('active')
+      }
+
+
+
+      // Xu li muted audio 
+      I('.btn-volumn').onclick = function () {
+         I('.btn-vol-on').classList.toggle('muted')
+         if (I('.btn-vol-on')) {
+            audio.muted = false // Khi vol on audio mac dinh
+         }
+         I('.btn-vol-off').classList.toggle('muted')
+         if (I('.btn-vol-off.muted')) {
+            audio.muted = true  // Khi vol on audio muted
+         }
+      }
+
+      // Xu li khi click vao item-nav 
+      II('.item-nav').forEach((item, index) => {
+         item.onclick = function () {
+            I('.item-nav.active').classList.remove('active')
+
+            item.classList.add('active')
+         }
+      })
+
+   }
+}
+function ranDom() {
+   let index = songNode.getAttribute('data-index') || Number(songNode.dataset.index)
+
+   playlists[index] = Math.floor(Math.random() * playlists.length)
+}
+
+renderPlayList()
+renderlistSong()
+loadingSong()
+renderDuration()
+app.handleSong()
